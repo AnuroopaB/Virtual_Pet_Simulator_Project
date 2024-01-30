faa844975655b9f86a304007414d67e466fb8208
Anuroopa <Anuroopa@ROOZ>
2024-01-30 16:48:05 -0500

    Code changes for Pet actions.

--- Virtual_Pet_Simulator_Project/Program.cs
+++ Virtual_Pet_Simulator_Project/Program.cs
@@ -27,7 +27,27 @@
             {
                 Console.Write($"\nCongrats, Your new buddy is a {a}.\n\nWhat would you like to call your new friend? ");
                 String Name = Console.ReadLine();
-                Console.WriteLine($"\nWelcome dear {Name}, we are so happy to be with you!");
+                Console.WriteLine($"\nWelcome dear {Name}, we are so happy to be with you!\n");
+                Console.Write($"Activities with {Name}:\n1. Feed {Name}\n2. Play with {Name}\n3. Let {Name} rest\n4. Check {Name}'s status\n5. Say bye to {Name}\n\nSelect the activity: ");
+                int activity = int.Parse(Console.ReadLine());
+                switch (activity)
+                {
+                    case 1:
+                        Console.WriteLine($"\nYou fed {Name}, {Name}'s hunger decreased, health increased.");
+                        break;
+                    case 2:
+                        Console.WriteLine($"\nYou played with {Name}, {Name}'s happiness increased, health decreased.");
+                        break;
+                    case 3:
+                        Console.WriteLine($"\n{Name} is resting, {Name}'s happiness decreased, health increased.");
+                        break;
+                    case 4:
+                        Console.WriteLine($"\nStatus of {Name}");
+                        break;
+                    case 5:
+                        Console.WriteLine($"\nBye, bye!!!");
+                        break;
+                }
             }
             else
             {
