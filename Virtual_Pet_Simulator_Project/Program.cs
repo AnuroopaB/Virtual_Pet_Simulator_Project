f7714a1a105b5d41130ae0b3174064f5aa2c95d8
Anuroopa <Anuroopa@ROOZ>
2024-01-29 23:51:34 -0500

    Add project files.

---
+++ Virtual_Pet_Simulator_Project/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace CSharpAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Available pets:");
+            Console.Write("1. Dog\n2. Cat\n3. Rabbit\n\nEnter your selection: ");
+            int selection = int.Parse(Console.ReadLine());
+            String a = "";
+            switch (selection)
+            {
+                case 1:
+                    a = "Dog";
+                    break;
+                case 2:
+                    a = "Cat";
+                    break;
+                case 3:
+                    a = "Rabbit";
+                    break;
+            }
+            if (a != "")
+            {
+                Console.WriteLine($"\nCongrats, Your new buddy is a {a}.");
+            }
+            else
+            {
+                Console.WriteLine("\nWrong selection!");
+            }
+        }
+    }
+}
