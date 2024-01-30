9520554dfbec2f7755316836b110501d0babb6fe
Anuroopa <Anuroopa@ROOZ>
2024-01-30 00:14:14 -0500

    Code for Pet welcome message.

--- Virtual_Pet_Simulator_Project/Program.cs
+++ Virtual_Pet_Simulator_Project/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Available pets:");
-            Console.Write("1. Dog\n2. Cat\n3. Rabbit\n\nEnter your selection: ");
+            Console.Write("1. Dog\n2. Cat\n3. Rabbit\n4. Parrot\n\nEnter your selection: ");
             int selection = int.Parse(Console.ReadLine());
             String a = "";
             switch (selection)
@@ -19,14 +19,20 @@
                 case 3:
                     a = "Rabbit";
                     break;
+                case 4:
+                    a = "Parrot";
+                    break;
             }
             if (a != "")
             {
-                Console.WriteLine($"\nCongrats, Your new buddy is a {a}.");
+                Console.Write($"\nCongrats, Your new buddy is a {a}.\n\nWhat would you like to call your new friend? ");
+                String Name = Console.ReadLine();
+                Console.WriteLine($"\nWelcome dear {Name}, we are so happy to be with you!");
             }
             else
             {
                 Console.WriteLine("\nWrong selection!");
+                //goto
             }
         }
     }
